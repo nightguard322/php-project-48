9aa10d62421201301891aab0ba4e1b4c5280649e
sander <sander_@inbox.ru>
2023-03-07 09:34:04 +0300

    added docopt format

--- src/showDoc.php
+++ src/showDoc.php
@@ -12,10 +12,12 @@ Generate diff
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
 DOCOPT;
 
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
