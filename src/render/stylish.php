e5a536ceb01a2c8dccffc1ea1b1c16d16e9f1f1e
sander <sander_@inbox.ru>
2024-02-17 16:51:18 +0300

    almost done except indents

--- src/render/stylish.php
+++ src/render/stylish.php
@@ -14,86 +14,42 @@ function stylish($diffObject)
             if ($current['children']) {
                 $preparedChildren = array_map(fn($child) => $iter($child), $current['children']);
                 $newChildren = array_reduce($preparedChildren, fn($acc, $child) => array_merge($acc, $child), []);
-                return [makeIndentWithKey('same', $currentKey) => $newChildren];
+                $result = [makeIndentWithKey('same', $currentKey) => $newChildren];
+                return $result;
             }
-            $indents = $current['status'] === 'changed' ? ['old', 'added'] : [$current['status']]; //nest => [key => value], nest => str, children => null, status => changed
-            //values = [[$key => $value], str]
-            //[[*spaces*key => value], str]
+            $indents = $current['status'] === 'changed' ? ['old', 'added'] : [$current['status']];
             $lines = [];
             for ($i = 0, $max = count($values); $i < $max; $i++) {
                 $key = makeIndentWithKey($indents[$i], $currentKey);
                 $lines[$key] = $iter($values[$i]);
             }
             return $lines;
-            // $lines = array_map(
-            //     fn($indent, $value) =>
-            //         makeIndentWithKey($indent, $depth, $currentKey), $iter($value, $depth + 1), // ['- buz' => buzz, '+ buz' => bars,  foo => bar]
-            //     $indents, 
-            //     $values
-            // );
-            /*
-            group1: {
-                - baz: bas
-                + baz: bars
-                    foo: bar
-                - nest: {
-                        key: value
-                    }
-                + nest: str
-                }
-
-                [group1 => 
-                    [
-                    - baz => bas,
-                    + baz => bars,
-                    foo => bar,
-                    - nest => [
-                        key => value
-                        ],
-                    + nest => str
-                    ];
-                ]
-            */
         } else {
             return $current;
-            // $lines = array_map(
-            //     fn($key, $value) =>
-            //         makeIndentWithKey($status, $depth, $key) . ': ' . $iter($value, $depth + 1),
-            //     array_keys($values), 
-            //     $values
-            // );
         };
     };
     $result = array_reduce(
-        $diffObject, fn($acc, $leaf) => array_merge($acc, [$iter($leaf, 1)]),
+        $diffObject, fn($acc, $leaf) => array_merge($acc, $iter($leaf, 1)),
         []
     );
-    return render($result);
-    
+    return render($result);  
 }
+
 function render(mixed $diffObject)
 {
     $iter = function ($current, $depth) use (&$iter) {
-        //var_dump($current);die;
         if (is_array($current)) {
-            $currentSpace = str_repeat(SPACE, INDENTCOUNT * $depth);
-            $preparedChildren = array_map(fn($child) => $iter($child, $depth * 2), $current); //вывод ребенка [0 => ключ: значение, 1 => ключ: значение]
-            $test = array_map(fn($key, $childTest) => "{$key}: {$childTest}", array_keys($current), $current);
-            var_dump($test);
-            $closetBracket = "{$currentSpace}}";
-            $lines = ['{', ...$preparedChildren, $closetBracket];
-                //var_dump($key, $lines);
-            //$result = "{$currentSpace}{$key} - тут ключ: " . implode("\n", $lines);
-            //return $result;
+            $currentIndentWidth = INDENTCOUNT * $depth;
+            $currentSpace = str_repeat(SPACE, $currentIndentWidth);
+            $closingBracket = str_repeat(SPACE, $currentIndentWidth - INDENTCOUNT);
+            $lines = array_map(fn($key, $child) => "{$currentSpace}{$key}: {$iter($child, $depth + 1)}", array_keys($current), $current);
+            $preparedLines = ['{', ...$lines, "{$closingBracket}}"];
+            return implode("\n", $preparedLines);
         }
             return $current;
     };
-    $lines = array_map(fn($child) => $iter($child, 1), $diffObject);
-    // print_r($diffObject);die;
-    // print_r($lines);
-    die;
-    // return implode("\n", ['{', ...$lines, '}']);
-    return 1;
+    $result = $iter($diffObject, 1);
+    return $result;
 }
 
 function makeIndentWithKey($status, $key)
@@ -101,7 +57,7 @@ function makeIndentWithKey($status, $key)
     $indentList = [
         'old' => '- ',
         'added' => '+ ',
-        'same' => ''
+        'same' => '  '
     ];
     $indent = $indentList[$status];
     return "{$indent}{$key}";
--- test.php
+++ test.php
@@ -1,15 +1,24 @@
 <?php
 
-function stringify(mixed $object): string
+use function Functional\retry;
+
+function stringify(mixed $object, string $indent = '', int $count = 1): string
 {
     if (!is_array($object)) {
         return toString($object);
     }
-    $iter = function($current, $depth) use (&$iter) {
-        
+    $iter = function($current, $depth) use (&$iter, $indent, $count) {
+        $currentIndentWidth = $count * $depth;
+        $currentIndent = str_repeat($indent, $currentIndentWidth);
+        $closedBracket = str_repeat($indent, $currentIndentWidth - $count);
+        if (is_array($current)) {
+            $lines = array_map(fn($key, $value) => "{$currentIndent}{$key}: {$iter($value, $depth + 1)}", array_keys($current), $current);
+            return implode("\n", ['{', ...$lines, "{$closedBracket}}"]);
+        }
+        return $current;
     };
-    $result = array_map(fn($element) => $iter($element, 1), $object);
-    return implode("\n", ["{", ...$result, "}"]);
+    $prepared = $iter($object, 1);
+    return $prepared;
 }
 
 function toString(mixed $object): string
@@ -22,8 +31,7 @@ function toString(mixed $object): string
 
 
 
-
-var_dump(stringify('hello')); // hello – значение приведено к строке, но не имеет кавычек
+// var_dump(stringify('hello')); // hello – значение приведено к строке, но не имеет кавычек
 stringify(true);    // true
 stringify(5);       // 5
 
@@ -40,4 +48,14 @@ stringify($data); // то же самое что stringify(data, ' ', 1);
 //  nested: {
 //   count: 5
 //  }
+// }
+
+var_dump(stringify($data, '|-', 2));
+// Символ, переданный вторым аргументом повторяется столько раз, сколько указано третьим аргументом.
+// {
+// |-|-hello: world
+// |-|-is: true
+// |-|-nested: {
+// |-|-|-|-count: 5
+// |-|-}
 // }
