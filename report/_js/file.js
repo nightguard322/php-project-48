7cab3b7fc11df79bd040df975f03c86492831101
sander <sander_@inbox.ru>
2024-05-04 21:41:58 +0300

    some fixes

--- report/_js/file.js
+++ report/_js/file.js
@@ -7,23 +7,23 @@
           $target.data('popover-hover', false);
 
           setTimeout(function () {
-          if (!$target.data('popover-hover')) {
-            $target.popover('hide');
-          }
+            if (!$target.data('popover-hover')) {
+                $target.popover('hide');
+            }
           }, 300);
       };
 
       $top_link.hide().click(function (event) {
-      event.preventDefault();
-      $body.animate({scrollTop:0}, 800);
+        event.preventDefault();
+        $body.animate({scrollTop:0}, 800);
       });
 
-    $window.scroll(function() {
-    if($window.scrollTop() > offset) {
-      $top_link.fadeIn();
-    } else {
-      $top_link.fadeOut();
-    }
+    $window.scroll(function (){
+        if($window.scrollTop() > offset) {
+          $top_link.fadeIn();
+        } else {
+          $top_link.fadeOut();
+        }
     }).scroll();
 
     $('.popin')
@@ -41,18 +41,18 @@
             }
 
       // show the popover
-          $container.popover('show');
+            $container.popover('show');
 
       // register mouse events on the popover
-          $target.next('.popover:not(.popover-initialized)')
-          .on({
-              'mouseenter': function () {
+            $target.next('.popover:not(.popover-initialized)')
+            .on({
+                'mouseenter': function () {
                     $target.data('popover-hover', true);
-            },
-          'mouseleave': function () {
-              hidePopover($container);
-            }
-       })
+                },
+                'mouseleave': function () {
+                    hidePopover($container);
+                }
+            })
             .addClass('popover-initialized');
         },
         'mouseleave.popover': function () {
