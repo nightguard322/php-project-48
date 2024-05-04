5119a243503d14e1f9bbf2097e91fb76a45fca95
sander <sander_@inbox.ru>
2024-05-04 21:16:47 +0300

    some fixes

--- tests/genDiffTest.php
+++ tests/genDiffTest.php
@@ -3,6 +3,7 @@
 namespace Differ\Differ\Tests;
 
 use PHPUnit\Framework\TestCase;
+
 use function Differ\Differ\genDiff;
 
 class GendiffTest extends TestCase
@@ -11,13 +12,9 @@ class GendiffTest extends TestCase
   
   public function testGendiff()
   {
-      // $expected = file_get_contents(__DIR__ . '/fixtures/resultWithNested.txt');
       $expected = file_get_contents(__DIR__ . '/fixtures/resultStylish.txt');
       $before = __DIR__ . '/fixtures/beforeWithNested.yml';
       $after = __DIR__ . '/fixtures/afterWithNested.yml';
       $this->assertEquals($expected, genDiff($before, $after));
   }
-
 }
-
-// 
--- tests/parserTest.php
+++ tests/parserTest.php
@@ -3,41 +3,41 @@
 namespace Differ\Differ\Tests;
 
 use PHPUnit\Framework\TestCase;
+
 use function Differ\Differ\parse;
 
 class ParserTest extends TestCase
 {
   private $res;
-  
   public function testParse()
   {
-    $file = __DIR__ . '/fixtures/beforeWithNested.yml';
-    $expected = [
-        "common" => [
-          "setting1" => "Value 1",
-          "setting2" => 200,
-          "setting3" => true,
-          "setting6" => [
-            "key" => "value",
-            "doge" => [
-              "wow" => ""
-            ]
-          ]
-        ],
-        "group1" => [
-          "baz" => "bas",
-          "foo" => "bar",
-          "nest" => [
-            "key" => "value"
-          ]
-        ],
-        "group2" => [
-          "abc" => 12345,
-          "deep" => [
-            "id" => 45
+  $file = __DIR__ . '/fixtures/beforeWithNested.yml';
+  $expected = [
+      "common" => [
+        "setting1" => "Value 1",
+        "setting2" => 200,
+        "setting3" => true,
+        "setting6" => [
+          "key" => "value",
+          "doge" => [
+            "wow" => ""
           ]
         ]
-        ];
+      ],
+      "group1" => [
+        "baz" => "bas",
+        "foo" => "bar",
+        "nest" => [
+          "key" => "value"
+        ]
+      ],
+      "group2" => [
+        "abc" => 12345,
+        "deep" => [
+          "id" => 45
+        ]
+      ]
+      ];
     $this->assertEquals($expected, parse($file));
   }
 
